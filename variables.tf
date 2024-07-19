5f20dd28fde25994f3de5a57043ed968683ba909
harsha5401 <98007205+harsha5401@users.noreply.github.com>
2024-07-19 11:14:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "rgname" {
+  type        = string
+  description = "Name rg group"
+  default     = "RGNAMEmlops"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the resources"
+  default     = "northeurope"
+}
+
+variable "ml" {
+  type        = string
+  description = "name of Ml workspace"
+  default     = "ML"
