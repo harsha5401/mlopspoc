9bcd77b18e8b0d55a0be251ec79057d9bfc28078
harsha5401 <98007205+harsha5401@users.noreply.github.com>
2024-07-19 10:18:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   features {}
 subscription_id ="9735d0c7-967d-40b3-becd-d1397f62c5b9"
-client_id = "a8db-48e8-9ded-2a3043458d42"
+client_id = "eb9f71da-a8db-48e8-9ded-2a3043458d42"
 client_secret = "2af0db4a-87b3-4e34-b2f5-c7fdaf435fc3"
 tenant_id = "c1d226ed-7a22-492c-a89e-50154dcb92fe"
 }
