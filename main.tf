8d34cf1495468bc5973cc346b0503919e70f3530
harsha5401 <98007205+harsha5401@users.noreply.github.com>
2024-07-24 08:30:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,12 @@ terraform {
 }
 provider "azurerm" {
   features {}
-subscription_id ="9735d0c7-967d-40b3-becd-d1397f62c5b9"
-client_id = "eb9f71da-a8db-48e8-9ded-2a3043458d42"
-client_secret = "TQP8Q~j8_tPEe_UOvxPOY1Y50YESBQHGMdHP.cJB"
-tenant_id = "c1d226ed-7a22-492c-a89e-50154dcb92fe"
+
+}
+data "azurerm_resource_group" "default" {
+  name = "${var.rgname}"
 }
 
-resource "azurerm_resource_group" "default" {
-  name     = var.rgname
-  location = var.location
-}
 resource "azurerm_application_insights" "default" {
   name                = "${var.ml}-appi"
   location            = var.location
